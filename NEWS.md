877e23bbd99ae691c4494b4054de2a95eccdb710
Martin Wilson <martin@pipegrep.co.uk>
2017-12-08 14:54:59 +0000

    test fix

--- NEWS.md
+++ NEWS.md
@@ -4,6 +4,9 @@ from 2 to 3.
 * Improved k-space spike detection plot limits for very stable data.
 * COG calculations are now performed on a thresholded image to reduce bias from
 inhomogenity.
+* Added coveralls checking.
+* Added a basic consistancy test.
+* Added an introduction vignette.
 
 # fmriqa 0.1.0
 * First public release.
--- tests/testthat/test_qa_measures.R
+++ tests/testthat/test_qa_measures.R
@@ -4,5 +4,7 @@ test_that("default analysis is consistent", {
   fname <- system.file("extdata", "qa_data.nii.gz", package = "fmriqa")
   res <- run_fmriqa(data_file = fname, gen_png = FALSE, gen_res_csv = FALSE,
                     tr = 3)
+
+  res$data_file <- NULL
   expect_equal_to_reference(res, "ref_results.rds", tolerance = 1e-6)
 })
