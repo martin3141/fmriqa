4784b8a323fde95545268638d5438127464e4a3d
Martin Wilson <martin@pipegrep.co.uk>
2017-12-11 10:56:41 +0000

    vignette prep

--- R/fmriqa.R
+++ R/fmriqa.R
@@ -4,6 +4,9 @@
 #' analysis protocol detailed by Friedman and Glover (2006)
 #' <doi:10.1002/jmri.20583>.
 #'
+#' #' To learn more about fmriqa, start with the vignettes:
+#' `browseVignettes(package = "fmriqa")`
+#'
 #' For a full list of functions:
 #' `help(package=fmriqa, help_type="html")`
 #'
