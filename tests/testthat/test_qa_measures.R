dea3b8f47e7e310de7088de6fa69e1750b4a911a
Martin Wilson <martin@pipegrep.co.uk>
2017-12-20 12:19:02 +0000

    moar tests

--- tests/testthat/test_qa_measures.R
+++ tests/testthat/test_qa_measures.R
@@ -7,3 +7,15 @@ test_that("default analysis is consistent", {
   res$data_file <- NULL
   expect_equal_to_reference(res, "ref_results.rds", tolerance = 1e-6)
 })
+
+test_that("pdf output runs", {
+  fname <- system.file("extdata", "qa_data.nii.gz", package = "fmriqa")
+  res <- run_fmriqa(data_file = fname, tr = 3, pdf_fname = tempfile(),
+                    gen_pdf = TRUE, gen_res_csv = FALSE, gen_png = FALSE)
+})
+
+test_that("spec csv output runs", {
+  fname <- system.file("extdata", "qa_data.nii.gz", package = "fmriqa")
+  res <- run_fmriqa(data_file = fname, tr = 3, spec_fname = tempfile(),
+                    gen_spec_csv = TRUE, gen_res_csv = FALSE, gen_png = FALSE)
+})
