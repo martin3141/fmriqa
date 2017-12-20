a827dd8adb5c768148193c5ca97245230f0e4eae
Martin Wilson <martin@pipegrep.co.uk>
2017-12-20 12:08:01 +0000

    improved testing

--- tests/testthat/test_qa_measures.R
+++ tests/testthat/test_qa_measures.R
@@ -2,9 +2,8 @@ context("qa_measures")
 
 test_that("default analysis is consistent", {
   fname <- system.file("extdata", "qa_data.nii.gz", package = "fmriqa")
-  res <- run_fmriqa(data_file = fname, gen_png = FALSE, gen_res_csv = FALSE,
-                    tr = 3)
-
+  res <- run_fmriqa(data_file = fname, tr = 3, png_fname = tempfile(),
+                    res_fname = tempfile(), spike_detect = TRUE)
   res$data_file <- NULL
   expect_equal_to_reference(res, "ref_results.rds", tolerance = 1e-6)
 })
